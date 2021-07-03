1faaa562d6f9ac413e51b4df4d25f78a73323933
ednoram <e.noramirian@gmail.com>
2021-07-03 15:34:33 +0400

    main

--- src/containers/RecoverPassword/index.tsx
+++ src/containers/RecoverPassword/index.tsx
@@ -1,20 +1,29 @@
 import { FC } from "react";
 
+import { useIsLoggedIn } from "@/hooks";
 import { AccountForm, SendEmailForm } from "@/components";
 
 import styles from "./RecoverPassword.module.scss";
 
 const RecoverPassword: FC = () => {
+  const isLoggedIn = useIsLoggedIn();
+
   return (
     <main>
       <section className={styles.content}>
         <h1 className={styles.content__title}>Recover Password</h1>
-        <div className="form_container">
-          <SendEmailForm recovery />
-        </div>
-        <div className={`${styles.content__password_form} form_container`}>
-          <AccountForm recoverPassword />
-        </div>
+        {isLoggedIn ? (
+          <p className="auth_problem_p">You are logged in</p>
+        ) : (
+          <>
+            <div className="form_container">
+              <SendEmailForm recovery />
+            </div>
+            <div className={`${styles.content__password_form} form_container`}>
+              <AccountForm recoverPassword />
+            </div>
+          </>
+        )}
       </section>
     </main>
   );
